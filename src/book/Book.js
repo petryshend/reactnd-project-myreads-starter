94b4409a882d0722a16ed89054aed56e02e80a19
dmytro.petryshen <dmytro.petryshen@vimeo.com>
2022-01-09 11:03:19 +0200

    Add loading state to book

--- src/book/Book.js
+++ src/book/Book.js
@@ -3,10 +3,20 @@ import {update} from "../BooksAPI";
 
 class Book extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: false
+        }
+    }
+
     updateBookShelf = (event) => {
+        this.setState({loading: true});
         const moveToShelf = event.target.value;
         update(this.props.book, moveToShelf)
             .then(() => {
+                this.setState({loading: false});
                 this.props.onBookMoved(this.props.book, moveToShelf);
             });
     }
@@ -15,7 +25,11 @@ class Book extends React.Component {
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(" ' + this.props.book.imageLinks.thumbnail + ' ")' }}></div>
+                    {this.props.book.imageLinks ? (
+                        <div className={"book-cover " + (this.state.loading ? 'loading' : '')} style={{ width: 128, height: 193, backgroundImage: 'url(" ' + this.props.book.imageLinks.thumbnail + ' ")' }}></div>
+                    ) : (
+                        <div className={"book-cover " + (this.state.loading ? 'loading' : '')} style={{ width: 128, height: 193, background: "lightgray" }}></div>
+                    )}
                     <div className="book-shelf-changer">
                         <select onChange={this.updateBookShelf} value={this.props.book.shelf ? this.props.book.shelf : 'none'}>
                             <option value="move" disabled>Move to...</option>
